a72973a3217f7f6bb49a73312f2b3c4f80b49a55
peppermintgummybears <peppermintgummybears@gmail.com>
2018-06-03 00:31:26 -0500

    moved older folders and added hackerrank working folder

--- pythonchallenge/03.py
+++ pythonchallenge/03.py
@@ -1,3 +1,18 @@
 #!/usr/local/bin/python3
 # Code for: Python Challenge 3
 # http://www.pythonchallenge.com/pc/def/ocr.html
+
+myFile = open("03.txt", 'r')
+lines = myFile.readlines()
+myFile.close()
+
+myStr = ''.join(lines)
+uniqueChars = set(myStr)
+
+charCount = { uc: myStr.count(uc) for uc in uniqueChars }
+
+rareChars = [ char for char, count in charCount.items() if charCount[char] == 1 ]
+rareChars.sort()
+print(rareChars)
+
+# Answer: equality
