908f9adb019347ffad4f988df1d1c28925fbfa65
peppermintgummybears <peppermintgummybears@gmail.com>
2018-06-10 17:55:34 -0500

    Submitted a second solution, simplifying the code a bit.

--- exercism/python/isogram/isogram.py
+++ exercism/python/isogram/isogram.py
@@ -1,24 +1,11 @@
-import re
-
 def is_isogram(string):
     testStr = string
 
-    # Strip out all non-word
-    rec = re.compile('[a-zA-z]{1}')
-    alphaOnly = ''.join(rec.findall(testStr))
+    # Strip out all non-word, and set to lowercase
+    alphaOnly = ''.join( [ c.lower() for c in testStr if c.isalpha()] )
 
-    # When just letters, convert to lowercase
-    alphaOnly = alphaOnly.lower()
-
-    # With just lowercase letters test for repeated characters
-    unique = set(alphaOnly)
-    orig = list(alphaOnly)
-
-    isIso = False
-    if ( len(unique) == len(orig) ):
-        isIso = True
-
-    return isIso
+    # Compare the length of the set (unique) vs original string length
+    return ( len(  set(alphaOnly) ) == len(  list(alphaOnly) ) )
 
 if __name__ == '__main__':
     is_isogram(input("Enter Text to Test: "))
