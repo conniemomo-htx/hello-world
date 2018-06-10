0ce6ec71f982beb790c525e59bbd99b57b584824
peppermintgummybears <peppermintgummybears@gmail.com>
2018-06-10 18:26:04 -0500

    Completed pangram exercise

--- exercism/python/pangram/pangram.py
+++ exercism/python/pangram/pangram.py
@@ -1,2 +1,13 @@
+import string
+
 def is_pangram(sentence):
-    pass
+    alphabet = string.ascii_lowercase
+
+    # Strip out all non-word, and set to lowercase
+    alphaOnly = [ c.lower() for c in sentence if c.isalpha() ]
+
+    # Compare the length of the set (unique) vs original string length
+    return ( len( alphabet ) == len(  set(alphaOnly) ) )
+
+if __name__ == '__main__':
+    is_pangram(input("Enter Sentence to Test: "))
